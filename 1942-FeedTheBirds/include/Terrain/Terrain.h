7b8f6c09e2872ad5f78ae86297e68892dce685cc
ElenisGit <lenpapad@gmail.com>
2016-02-03 14:20:58 +0200

    fix actionPoints

--- 1942-FeedTheBirds/include/Terrain/Terrain.h
+++ 1942-FeedTheBirds/include/Terrain/Terrain.h
@@ -18,6 +18,7 @@ private:
 
 	int textAlign;
 	bool textVisible;
+	int numOfScrolls;
 
 	/* discriminate background from terrain objects */
 	float terrainWidth;
--- 1942-FeedTheBirds/src/Terrain/Terrain.cpp
+++ 1942-FeedTheBirds/src/Terrain/Terrain.cpp
@@ -6,7 +6,7 @@ using namespace std;
 	font_file("1942-FeedTheBirds\\data\\Fonts\\karmatic_arcade_font.ttf"),
 	width(SCREEN_WINDOW_WIDTH),
 	height(SCREEN_WINDOW_WIDTH),
-	x(0), y(0), velX(3), velY(0), dirX(-1), dirY(1)
+	x(0), y(0), velX(3), velY(0), dirX(-1), dirY(1), numOfScrolls(0)
 {
 	bgImage = al_load_bitmap("1942-FeedTheBirds\\data\\Bitmaps\\terrain\\background5.png");
 	remainingLivesImage = al_load_bitmap("1942-FeedTheBirds\\data\\Bitmaps\\life.png");
@@ -20,7 +20,7 @@ using namespace std;
 		al_show_native_message_box(NULL, "Error", NULL, "failed to load font file!\n", NULL, NULL);
 		return;
 	}
-	bright_green = al_map_rgba_f(0.5, 1.0, 0.5, 1.0);
+	bright_green = al_map_rgba_f(0.8, 0.0, 0.0, 1.0);
 	grey = al_map_rgba_f(0.7, 0.7, 0.7, 1.0);
 }
 
@@ -28,13 +28,16 @@ Terrain::~Terrain() {
 }
 
 float Terrain::getTerrainX() {
-	return -((this->x)/3);
+	//cout << "imageX: " << (numOfScrolls*SCREEN_WINDOW_WIDTH - (this->x)) / 3;
+	return (numOfScrolls*SCREEN_WINDOW_WIDTH -(this->x))/3;
 }
 
 void Terrain::updateBackground() {
 	this->x += this->velX * this->dirX;
-	if (this->x + this->width <= 0)
+	if (this->x + this->width <= 0) {
+		numOfScrolls++;
 		this->x = 0;
+	}
 }
 
 void Terrain::drawBackground(int score, int highscore, int lives, int loops) {
@@ -43,14 +46,14 @@ void Terrain::drawBackground(int score, int highscore, int lives, int loops) {
 
 	al_draw_bitmap(this->bgImage, this->x, this->y, 0);
 
-	if (this->x + this->width < SCREEN_WINDOW_WIDTH)
-		al_draw_bitmap(this->bgImage, this->x+this->width, this->y, 0);
-
+	if (this->x + this->width < SCREEN_WINDOW_WIDTH) {
+		al_draw_bitmap(this->bgImage, this->x + this->width, this->y, 0);
+	}
 	//al_draw_bitmap(this->bgImage, x, y, false);
 	al_draw_bitmap(scoreImage, 40, 10, false);
-	//al_draw_text(font2, bright_green, 220, 10, ALLEGRO_ALIGN_CENTER, to_string(score).c_str());
+	al_draw_text(font2, bright_green, 220, 10, ALLEGRO_ALIGN_CENTER, to_string(score).c_str());
 	al_draw_bitmap(highScoreImage, 420, 10, false);
-	//al_draw_text(font1, bright_green, 700, 10, ALLEGRO_ALIGN_CENTER, to_string(highscore).c_str());
+	al_draw_text(font1, bright_green, 700, 10, ALLEGRO_ALIGN_CENTER, to_string(highscore).c_str());
 
 	int livesImgWidth = al_get_bitmap_width(remainingLivesImage);
 
